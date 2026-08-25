f7b624040553e33e2dca244ffce7bbd610b4aebb
mrsohagmia32-cell <mrsohagmia32@gmail.com>
2026-08-25 23:42:01 +0600

    Remove GITHUB_TOKEN from config.js
    Removed sensitive GITHUB_TOKEN from configuration.

--- config.js
+++ config.js
@@ -2,6 +2,5 @@ const CONFIG = {
     GITHUB_USERNAME: "mrsohagmia32-cell",
     REPO_NAME: "bds-26news", // আপনার নতুন রিপোজিটরির নাম এখানে বসিয়ে দেবেন
     FILE_PATH: "posts.json",
-    GITHUB_TOKEN: "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",
     ADMIN_PASS: "Sa28747228@"
 };
